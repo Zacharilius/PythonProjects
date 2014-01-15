111aab1899ab2c31cad304656eec1dfb866f0e00
zacharilius <zabensley@gmail.com>
2014-01-14 19:27:38 -0500

    Started encode method

--- Text/vigenereVernamCaesar_Ciphers.py
+++ Text/vigenereVernamCaesar_Ciphers.py
@@ -5,25 +5,39 @@ Uses a vigenere cipher to encode and decode plain text
 """
 
 class VigenereCipher:
+    #Creates a vigener table
     def createVigenereTable(self):
         vigenereTable = []
         for arr in xrange(26):
             vigenereTable.append([])
         x = 0
+        letterStart = 65
         for arr in vigenereTable:
-            offset = 26
+            string = ""
             for row in xrange(26):
-                if 65 + row >= 65 + offset:
-                    letter = str(chr(65 + row - 25))
+                if letterStart + row > 90:
+                    letter = str(chr(letterStart + row - 26))
                 else:
-                    letter = str(chr(65 + row))
+                    letter = str(chr(letterStart + row))
                 vigenereTable[x].append(letter)
+                string += " " + letter
+            letterStart += 1
             x += 1
-        print vigenereTable
+            print string
+    def createPassPhrase(self,word):
+        pass
+
+    def encode(self, wordToEncode,passPhrase):
+        if len(wordToEncode) < len(passPhrase):
+            print "Yep"
         
+    def decode(self, wordToDecode, passPhrase):
+        pass
+                
     def __init__(self):
         self.data = []
 
 
 vig = VigenereCipher()
 vig.createVigenereTable()
+vig.encode("yep", "nope")
