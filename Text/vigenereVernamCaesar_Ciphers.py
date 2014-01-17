694ec524701c3b57148ea85b626e37367af6ea73
zacharilius <zabensley@gmail.com>
2014-01-17 09:48:20 -0500

    Finished VigenereVernam Cipher

--- Text/vigenereVernamCaesar_Ciphers.py
+++ Text/vigenereVernamCaesar_Ciphers.py
@@ -26,7 +26,7 @@ class VigenereCipher:
             print string
         return vigenereTable
             
-    def createPassPhrase(self,wordToEncodeOrDecode, passPhrase):
+    def createPassPhrase(self, wordToEncodeOrDecode, passPhrase):
         #creates a passphrase the same length as the word to encode, if necessary
         count = 0
         while len(wordToEncodeOrDecode) > len(passPhrase):
@@ -34,7 +34,9 @@ class VigenereCipher:
                 count = 0
             passPhrase += passPhrase[count]
             count += 1
-
+        return passPhrase
+    
+    def createPassPhraseNum(self,wordToEncodeOrDecode, passPhrase):
         #creates an array of characters in integer representation of the wordToEncode and the passPhrase
         #Stops encoding if not all letter and returns "Unrecognized input"
         encodedWord = ""
@@ -45,13 +47,13 @@ class VigenereCipher:
             if encNum < 91 and encNum >= 65:
                 charEncNum.append(encNum)
             else:
-                return "Unreconized input"
+                return "Unreconized input 1"
         for letter in passPhrase:
             passNum = ord(letter)
             if passNum < 91 and passNum >= 65:
                 charPassNum.append(passNum)
             else:
-                return "Unrecognized input"
+                return "Unrecognized input 2"
         return (charEncNum, charPassNum)
 
     def encode(self, wordToEncode,passPhrase, vigenereTable):
@@ -62,7 +64,7 @@ class VigenereCipher:
         print "Word to encode: " + wordToEncode
         print "Pass phrase: " + passPhrase
 
-        createPassPhraseOutput = self.createPassPhrase(wordToEncode, passPhrase)
+        createPassPhraseOutput = self.createPassPhraseNum(wordToEncode, self.createPassPhrase(wordToEncode, passPhrase))
         charEncNum, charPassNum = createPassPhraseOutput
 
         #Reference the 2d array aka the vigenere table to encode the word or phrase
@@ -75,25 +77,35 @@ class VigenereCipher:
         
         
     def decode(self, wordToDecode, passPhrase, vigenereTable):
+        #strips spaces and turns into uppercase
+        wordToDecode = wordToDecode.strip().upper()
+        passPhrase = passPhrase.strip().upper()
         print "Word to decode: " + wordToDecode
-        print "Pass phrase: " + wordToEncode
-        createPassPhraseOutput = self.createPassPhrase(wordToEncode, passPhrase)
+        print "Pass phrase: " + passPhrase
+
+        createPassPhraseOutput = self.createPassPhraseNum(wordToDecode, self.createPassPhrase(wordToDecode, passPhrase))
+        charEncNum, charPassNum = createPassPhraseOutput
         
         count = 0
-        encodedOutput = ""
+        decodedOutput = ""
         while count < len(charEncNum):
-            encodedOutput += vigenereTable[charEncNum[count]-65][charPassNum[count]-65]
+            #find correct column based on pass phrase letter
+            column = vigenereTable[charPassNum[count]-65]
+
+            decodedOutput += vigenereTable[0][column.index(wordToDecode[count])]
+            #encodedOutput += vigenereTable[charEncNum[count]-65][self.createPassPhrase().index(passPhrase[count])] #PROBLEM
             count += 1
-        return encodedOutput
-                        
+        return decodedOutput
+        #then search using .index() to to find letter from encoded word in column
+        #then add column 0 and row of .index() to translated word
     def __init__(self):
         self.data = []
         
     def main(self):
         vigenereTable = vig.createVigenereTable()
-        encodedWord = vig.encode("nope ", "yep ", vigenereTable)
+        encodedWord = vig.encode("alphabet ", "yep ", vigenereTable)
         print "Encoded word: " + encodedWord
-        print "Decoded word: " + vig.encode(encodedWord, "yep ", vigenereTable)
+        print "Decoded word: " + vig.decode(encodedWord, "yep ", vigenereTable)
 
 
 vig = VigenereCipher()
